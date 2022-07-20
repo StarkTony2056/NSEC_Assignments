4bf89489fa1477806bfedc1acd79708f7d139ee6
Mainak Chakraborty <91143608+StarkTony2056@users.noreply.github.com>
2022-07-20 20:19:40 +0530

    Final Update DSA_1.c

--- DSA_1.c
+++ DSA_1.c
@@ -2,7 +2,8 @@
 #include<stdio.h>
 
 int main() {
-  int r, c, i, j, count, r_tmp, c_tmp;
+  int r, c, i, j, count, r_tmp;
+  count = 0;
   //taking input
   printf("Enter number of rows: ");
   scanf("%d", &r);
@@ -10,14 +11,14 @@ int main() {
   scanf("%d", &c);
   int a[r][c];
   printf("Enter elements of array row-wise:\n");
-  for(i=0; i<r; i++) {
+  for(i=0; i<r; i++) 
     for(j=0; j<c; j++) {
       scanf("%d", &a[i][j]);
       if(a[i][j]>0)
         count++;
     }
-  }
   //printing input array
+  printf("The given sparse matrix is:\n");
   for(i=0; i<r; i++) {
     for(j=0; j<c; j++) {
       printf("%d\t", a[i][j]);
@@ -28,22 +29,21 @@ int main() {
   int b[count+1][3];
   //operating on content of b[][]
   r_tmp=1;
-  for(i=0; i<r; i++) {
-    for(j=0; j<c; j++) {
+  for(i=0; i<r; i++)
+    for(j=0; j<c; j++)
       if(a[i][j]>0) {
-        c_tmp=0;
-        b[r_tmp][c_tmp++]=i;
-        b[r_tmp][c_tmp++]=j;
-        b[r_tmp][c_tmp++]=a[i][j];
+        b[r_tmp][0]=i;
+        b[r_tmp][1]=j;
+        b[r_tmp][2]=a[i][j];
         r_tmp++;
       }
-    }
-  }
+  
   //putting values in zeroth row of b[][]
   b[0][0]=r;
   b[0][1]=c;
   b[0][2]=count;
   //printing final array
+  printf("The reduced sparse matrix is:\n");
   for(i=0; i<count+1; i++) {
     for(j=0; j<3; j++)
       printf("%d\t", b[i][j]);
